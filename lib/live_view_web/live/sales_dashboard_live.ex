5aa238348faafe3f6be19047d3fbd00870be443b
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-08-09 15:30:43 -0500

    Update sales dashboard template

--- lib/live_view_web/live/sales_dashboard_live.ex
+++ lib/live_view_web/live/sales_dashboard_live.ex
@@ -15,6 +15,40 @@ defmodule LiveViewWeb.SalesDashboardLive do
 
   def render(assigns) do
     ~L"""
+    <h1>Sales Dashboard</h1>
+    <div id="dashboard">
+      <div class="stats">
+        <div class="stat">
+          <span class="value">
+            <%= @new_orders %>
+          </span>
+          <span class="name">
+            New Orders
+          </span>
+        </div>
+        <div class="stat">
+          <span class="value">
+            $<%= @sales_amount %>
+          </span>
+          <span class="name">
+            Sales Amount
+          </span>
+        </div>
+        <div class="stat">
+          <span class="value">
+            <%= @satisfaction %>%
+          </span>
+          <span class="name">
+            Satisfaction
+          </span>
+        </div>
+      </div>
+
+      <button phx-click="refresh">
+        <img src="images/refresh.svg" alt="refresh-image">
+        Refresh
+      </button>
+    </div>
     """
   end
 end
